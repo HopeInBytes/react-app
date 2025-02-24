bd134cdb679236449b777975831a471f2b268c0b
HopeInBytes <pratikshasable95@gmail.com>
2025-02-24 15:43:37 +0530

    Understanding the react flow and structure

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Hope from "./hope"
+
+function App() {
+
+  return (
+  <>
+  <Hope/>
+  <h1>vite react</h1>
+  </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/hope.jsx
@@ -0,0 +1,6 @@
+function Hope(){
+    return (
+        <h2>try try but don't cry!</h2>
+    )
+}
+export default Hope
