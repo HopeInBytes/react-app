8958853dd486d9cb713c243c623811ad03607377
HopeInBytes <pratikshasable95@gmail.com>
2025-03-01 15:06:29 +0530

    Hooks and state

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+function App(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
 createRoot(document.getElementById('root')).
 render(
 
